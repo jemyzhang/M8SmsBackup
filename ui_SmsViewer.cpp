e466d48ffb440460548c95801616cddfe7575743
jemyzhang <jemyzhang@d8a46e4c-c5ba-c94b-a8e5-e0e26589f33e>
2009-09-22 14:10:56 +0000

    增加回复，转发，举报功能
    git-svn-id: https://172.16.185.65/svn/M8SmsBackup/trunk@33 d8a46e4c-c5ba-c94b-a8e5-e0e26589f33e

--- ui_SmsViewer.cpp
+++ ui_SmsViewer.cpp
@@ -87,29 +87,29 @@ BOOL Ui_SmsViewerWnd::OnInitDialog() {
 	m_Content.SetTextColor( RGB(255,255,255));
 	m_Background.AddChild(&m_Content);
 
-	int btnX = GetWidth() - 20 - 48;
-	int btnY = y;
-	m_ForwardSms.SetPos(btnX, btnY, 48, 48);
-	m_ForwardSms.SetButtonType(MZC_BUTTON_DOWNLOAD);
+	int btnX = GetWidth() - 10 - 48;
+	int btnY = y + 5;
+	m_ForwardSms.SetPos(btnX, btnY, 55, 55);
+	m_ForwardSms.SetButtonType(MZC_BUTTON_NONE);
 	m_ForwardSms.SetID(MZ_IDC_BUTTON_FORWARD);
 	m_ForwardSms.SetImage_Normal(pimg[IDB_PNG_FORWARD_SMS - IDB_PNG_BEGIN]);
 	m_ForwardSms.SetImage_Pressed(pimg[IDB_PNG_FORWARD_SMS - IDB_PNG_BEGIN]);
 	m_Background.AddChild(&m_ForwardSms);
 
-	btnY += 48;
-	m_ReplySms.SetPos(btnX, btnY, 48, 48);
-	m_ReplySms.SetButtonType(MZC_BUTTON_DOWNLOAD);
+	btnY += 60;
+	m_ReplySms.SetPos(btnX, btnY, 55, 55);
+	m_ReplySms.SetButtonType(MZC_BUTTON_NONE);
 	m_ReplySms.SetID(MZ_IDC_BUTTON_REPLY);
 	m_ReplySms.SetImage_Normal(pimg[IDB_PNG_REPLY_SMS - IDB_PNG_BEGIN]);
 	m_ReplySms.SetImage_Pressed(pimg[IDB_PNG_REPLY_SMS - IDB_PNG_BEGIN]);
 	m_Background.AddChild(&m_ReplySms);
 
-	btnY += 48;
-	m_ReportSms.SetPos(btnX, btnY, 48, 48);
-	m_ReportSms.SetButtonType(MZC_BUTTON_DOWNLOAD);
+	btnY += 60;
+	m_ReportSms.SetPos(btnX, btnY, 55, 55);
+	m_ReportSms.SetButtonType(MZC_BUTTON_NONE);
 	m_ReportSms.SetID(MZ_IDC_BUTTON_REPORT);
-	m_ReportSms.SetImage_Normal(pimg[IDB_PNG_FORWARD_SMS - IDB_PNG_BEGIN]);
-	m_ReportSms.SetImage_Pressed(pimg[IDB_PNG_FORWARD_SMS - IDB_PNG_BEGIN]);
+	m_ReportSms.SetImage_Normal(pimg[IDB_PNG_REPORT_SMS - IDB_PNG_BEGIN]);
+	m_ReportSms.SetImage_Pressed(pimg[IDB_PNG_REPORT_SMS - IDB_PNG_BEGIN]);
 	m_Background.AddChild(&m_ReportSms);
 
 	m_Toolbar.SetPos(0, GetHeight() - MZM_HEIGHT_TEXT_TOOLBAR, GetWidth(), MZM_HEIGHT_TEXT_TOOLBAR);
@@ -126,6 +126,48 @@ BOOL Ui_SmsViewerWnd::OnInitDialog() {
 void Ui_SmsViewerWnd::OnMzCommand(WPARAM wParam, LPARAM lParam) {
 	UINT_PTR id = LOWORD(wParam);
 	switch (id) {
+		case MZ_IDC_BUTTON_FORWARD:	//转发
+			{
+				MzBeginWaitDlg(m_hWnd);
+				SmsSimpleData_ptr pr = precord + viewIndex;
+				PROCESS_INFORMATION pi;
+				CMzString s = L"-n ";
+				s = s + L"▓";
+				s = s + pr->Content;
+				CreateProcess(L"\\windows\\sms.exe", 
+					s.C_Str() , NULL, NULL, FALSE, 0, NULL, NULL, NULL, &pi);
+				MzEndWaitDlg();
+				break;
+			}
+		case MZ_IDC_BUTTON_REPLY:	//回复
+			{
+				MzBeginWaitDlg(m_hWnd);
+				SmsSimpleData_ptr pr = precord + viewIndex;
+				PROCESS_INFORMATION pi;
+				CMzString s = L"-n ";
+				s = s + pr->MobileNumber;
+				s = s + L"▓";
+				CreateProcess(L"\\windows\\sms.exe", 
+					s.C_Str() , NULL, NULL, FALSE, 0, NULL, NULL, NULL, &pi);
+				MzEndWaitDlg();
+				break;
+			}
+		case MZ_IDC_BUTTON_REPORT:	//举报
+			{
+				MzBeginWaitDlg(m_hWnd);
+				SmsSimpleData_ptr pr = precord + viewIndex;
+				PROCESS_INFORMATION pi;
+				CMzString s = L"-n ";
+				s = s + L"10086999";
+				s = s + L" ▓";
+				s = s + pr->MobileNumber;
+				s = s + L" ";
+				s = s + pr->Content;
+				CreateProcess(L"\\windows\\sms.exe", 
+					s.C_Str() , NULL, NULL, FALSE, 0, NULL, NULL, NULL, &pi);
+				MzEndWaitDlg();
+				break;
+			}
 		case MZ_IDC_TOOLBAR_MAIN:
 			{
 				int nIndex = lParam;
